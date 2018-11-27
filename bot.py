8cda5afcee0f1494b65302376720cf8f6b8765e0
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-11-27 16:04:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -638,10 +638,6 @@ async def presence():
         await sleep(30)
         await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name="with cats || ,help"))
         await sleep(30)
-        await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.listening, name=f"{len(bot.users)} users || ,help"))
-        await sleep(30)
-        await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(bot.guilds)} servers || ,help"))
-        await sleep(30)
 	
 
 bot.loop.create_task(presence())
